b0f59f9c2762b7aa1cfe4d1a95e5ec82bec9d1cb
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-05-29 21:58:16 -0300

    [feature] - implementado lista de favoritos

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '../../services/character.service';
+import { Character } from '../../shared/models/Character.model';
+import { Observable } from 'rxjs';
+import { CardComponent } from '../../components/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './favorites.component.html',
-  styleUrl: './favorites.component.scss'
+  styleUrl: './favorites.component.scss',
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit {
+  constructor(private characterService: CharacterService) {}
 
+  characters$: Observable<Character[]>;
+
+  ngOnInit(): void {
+    this.characters$ = this.characterService.favorites$;
+  }
+
+  removeFromFavorite(char: Character) {
+    this.characterService.removeFromFavorites(char.id);
+  }
 }
