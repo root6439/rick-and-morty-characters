b5bbe23f8cb3968f94f98156ba16b0e3d23c4356
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-06-01 13:46:43 -0300

    [fix] - ajustado html do card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
